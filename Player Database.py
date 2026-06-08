bef8e61f244d094000e0809641d4fea5f602adad
Arnav Shankar <shankar.arnav@gmail.com>
2026-06-08 09:47:31 -0400

    Create Player Database.py

---
+++ Player Database.py
@@ -0,0 +1,28 @@
+player1 = {
+    "name" : "Shai Gilgeous Alexander",
+    "ppg" : 32.7,
+    "salary" : 35860000
+}
+player2 = {
+    "name" : "Nikola Jokic",
+    "ppg" : 27.7,
+    "salary" : 55220000
+}
+player3 = {
+    "name" : "Victor Wembanyama",
+    "ppg" : 25.0,
+    "salary" : 13793692
+}
+player4 = {
+    "name" : "Luka Doncic",
+    "ppg" : 33.5,
+    "salary" : 53460000
+}
+player5 = {
+    "name" : "Cade Cunningham",
+    "ppg" : 26.1,
+    "salary" : 53817000
+}
+players = [player1, player2, player3, player4, player5]
+for player in players:
+    print(player)
